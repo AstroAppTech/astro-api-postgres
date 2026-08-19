a558d80adbbef2730eeefc82c9903316c519ea19
rosamaduda <rosamaduda26@gmail.com>
2026-08-18 21:43:08 -0300

    feat(KNBN-123): criando o projeto

---
+++ src/main/java/com/astro/postgres/AstroPostgresApiApplication.java
@@ -0,0 +1,13 @@
+package com.astro.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroPostgresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroPostgresApiApplication.class, args);
+	}
+
+}
